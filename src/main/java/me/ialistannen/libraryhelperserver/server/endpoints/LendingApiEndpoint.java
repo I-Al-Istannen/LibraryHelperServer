71ebec24528a9f493905f530afa34f439159c481
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2017-09-05 22:57:53 +0200

    Added a proper delete method and removed the abomination I had before
    * I have no idea why I decided that is a nice name, but it isn't like
      anybody will read the code anyways

--- src/main/java/me/ialistannen/libraryhelperserver/server/endpoints/LendingApiEndpoint.java
+++ src/main/java/me/ialistannen/libraryhelperserver/server/endpoints/LendingApiEndpoint.java
@@ -4,9 +4,6 @@ import com.google.gson.JsonObject;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Methods;
-import java.lang.reflect.Field;
-import java.util.Map;
-import me.ialistannen.isbnlookuplib.book.Book;
 import me.ialistannen.isbnlookuplib.book.BookDataKey;
 import me.ialistannen.isbnlookuplib.isbn.Isbn;
 import me.ialistannen.isbnlookuplib.isbn.IsbnConverter;
@@ -19,16 +16,12 @@ import me.ialistannen.libraryhelperserver.db.elastic.queries.QueryByIsbn;
 import me.ialistannen.libraryhelperserver.server.utilities.Exchange;
 import me.ialistannen.libraryhelperserver.server.utilities.HttpStatusSender;
 import me.ialistannen.libraryhelperserver.util.MapBuilder;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 /**
  * Allows the user to lend books.
  */
 public class LendingApiEndpoint implements HttpHandler {
 
-  private static final Logger LOGGER = LogManager.getLogger(LendingApiEndpoint.class);
   private static final BookDataKey BORROWER_KEY = new StringBookDataKey("BORROWER");
 
   private IsbnConverter isbnConverter;
@@ -88,23 +81,11 @@ public class LendingApiEndpoint implements HttpHandler {
     } else if (exchange.getRequestMethod().equals(Methods.DELETE)) {
       if (bookOptional.isPresent()) {
         LoanableBook loanableBook = bookOptional.get();
-        // FIXME: 05.09.17 Add real method
-        careFuckAllDelete(loanableBook, BORROWER_KEY);
+        loanableBook.removeData(BORROWER_KEY);
         databaseMutator.addBook(loanableBook);
       }
 
       Exchange.body().sendJson(exchange, MapBuilder.of("deleted", true).build());
-    }
-  }
-
-  private void careFuckAllDelete(LoanableBook book, BookDataKey key) {
-    try {
-      Field field = Book.class.getDeclaredField("dataMap");
-      field.setAccessible(true);
-      Map<?, ?> dataMap = (Map<?, ?>) field.get(book);
-      dataMap.remove(key);
-    } catch (IllegalAccessException | NoSuchFieldException e) {
-      LOGGER.log(Level.WARN, "My crap blew up", e);
     }
   }
 }
